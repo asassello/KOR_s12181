512481825ea6636abaff0c822015700af27019fc
user <user@user-THINK>
2014-12-09 12:08:50 +0100

    ant build added

---
+++ KOR_s12181_2/src/edu/pjwstk/kor/model/data/DataGenerator.java
@@ -0,0 +1,35 @@
+package edu.pjwstk.kor.model.data;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.config.Configuration;
+import com.db4o.config.ObjectClass;
+import com.db4o.ext.ExtObjectContainer;
+
+import edu.pjwstk.kor.model.Adress;
+import edu.pjwstk.kor.model.Employee;
+import edu.pjwstk.kor.model.Package;
+import edu.pjwstk.kor.model.Payment;
+import edu.pjwstk.kor.model.PaymentType;
+import edu.pjwstk.kor.model.Person;
+import edu.pjwstk.kor.model.Receiver;
+import edu.pjwstk.kor.model.Sender;
+import edu.pjwstk.kor.model.Shipment;
+import edu.pjwstk.kor.model.Person;
+import edu.pjwstk.kor.model.Status;
+
+public class DataGenerator {
+
+}
